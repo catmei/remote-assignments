bbd4c0b1b0669126045a326ff17ae5f80aa02d92
catvin desktop <b02202040@gmail.com>
2023-07-08 18:15:21 +0800

    revise w1 binary search

--- Week-1/Assignment-4/assignment_4.py
+++ Week-1/Assignment-4/assignment_4.py
@@ -2,8 +2,8 @@ def binary_search_position(numbers, target):
     min_index = 0
     max_index = len(numbers)-1
 
-    while min_index < max_index:
-        mid_index = max_index+min_index//2
+    while min_index <= max_index:
+        mid_index = (max_index+min_index)//2
         mid_number = numbers[mid_index]
 
         if target == mid_number:
